7a540172933657f27b232b46f9d4e57da85978d0
Edgar <edbarkhudaryan@gmail.com>
2023-05-16 17:11:30 +0400

    Phonebook app init

---
+++ Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PhoneBook
+{
+	public class Contact
+	{
+        public string Name { get; set; }
+        public string LastName { get; set; }
+		public string Separator { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Contact(string name,string phoneNumber, string separator, string lastName = null)
+		{
+			this.Name = name;
+			this.LastName = lastName;
+			this.Separator = separator;
+			this.PhoneNumber = phoneNumber;
+		}
+
+        public override string ToString()
+        {
+            return " " + Name + " " + LastName+ " "+ Separator+ " "+ PhoneNumber;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using PhoneBook.Enums;
+using PhoneBook.Utils;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello your PhoneBook");
+
+            Console.WriteLine("Find the files here: PhoneBook.txt}");
+
+
+
+            //string filePath = @"C:\path\to\your\file.txt";
+
+            string filePath= Console.ReadLine();
+            //  string[] lines = ReadAllLinesFromFile(filePath);
+
+            var path=  PathUtil.GetFilePath("PhoneBook.txt");
+            PersonContactService personContact = new PersonContactService();
+            var contact=   personContact.ImportPersonContacts(path);
+
+     
+            MenuOrder();
+            string menuType = Console.ReadLine();
+
+            MenuOrder menuOrder = TypeCriteriaMapper.ReadMenuOrder(menuType[0].ToString().ToUpper()+ menuType.Substring(1).ToLower());
+
+
+            Criteria();
+
+            string criteriaType = Console.ReadLine();
+            CriteriaOrder criteriaOrder = TypeCriteriaMapper.ReadOrderCreteria(criteriaType[0].ToString().ToUpper()+ criteriaType.Substring(1).ToLower());
+ 
+
+            personContact.FindALlPersonContact(menuOrder: menuOrder, criteria: criteriaOrder, contacts: contact);
+            Console.ReadLine();
+        }
+
+        private static void MenuOrder()
+        {
+            Console.WriteLine("Please choose an ordering to sort: “Ascending” or “Descending");
+
+          
+        }
+
+        private static void Criteria()
+        {
+            Console.WriteLine("Please choose criteria: “Name”, “Surname” or “PhoneNumberCode");
+        }
+
+    }
+}
+
